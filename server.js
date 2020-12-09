3c5a30172bc63e39102cb4b3f5b75f7d593fa152
GnanaPradeepRS <gnana.gundluru@riversand.com>
2020-12-09 16:40:19 +0530

    changed payload size

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 const app = express()
 
-app.use(bodyParser.json())
+
+app.use(bodyParser.json({limit: '10mb', extended: true}))
 
 app.post('/api/warnings', (req, res) => {
     let request = JSON.parse(JSON.stringify(req.body.params.businessRules));
@@ -45,8 +46,7 @@ app.post('/api/all', (req, res) => {
     let requestValues = Object.values(request);
     let errorFlag = 0;
     let obj = {
-        businessRules : {},
-        status : ''
+        businessRules : {}
     }
 
     for (let i = 0; i < requestKeys.length; i++) {
@@ -127,173 +127,173 @@ const controller = (br) => {
                     }
                     break;
                 
-                case "GetWhereUsedEntityIds":
-                    let getWhereUsedEntityIds = GetWhereUsedEntityIds(br);
-                    if (getWhereUsedEntityIds !== null) {
-                        obj["GetWhereUsedEntityIds"] = getWhereUsedEntityIds;   
-                    }
-                    break;
+                // case "GetWhereUsedEntityIds":
+                //     let getWhereUsedEntityIds = GetWhereUsedEntityIds(br);
+                //     if (getWhereUsedEntityIds !== null) {
+                //         obj["GetWhereUsedEntityIds"] = getWhereUsedEntityIds;   
+                //     }
+                //     break;
 
-                case "IsEntityInWorkflow":
-                    let isEntityInWorkflow = keywordCountCommonMethod(br , 'IsEntityInWorkflow' , 1);
-                    if (isEntityInWorkflow !== null) {
-                        obj["IsEntityInWorkflow"] = isEntityInWorkflow;   
-                    }
-                    break;
+                // case "IsEntityInWorkflow":
+                //     let isEntityInWorkflow = keywordCountCommonMethod(br , 'IsEntityInWorkflow' , 1);
+                //     if (isEntityInWorkflow !== null) {
+                //         obj["IsEntityInWorkflow"] = isEntityInWorkflow;   
+                //     }
+                //     break;
 
-                case "IsEntityInWorkflowInContext":
-                    let isEntityInWorkflowInContext = keywordCountCommonMethod(br , 'IsEntityInWorkflowInContext' , 1);
-                    if (isEntityInWorkflowInContext !== null) {
-                        obj["IsEntityInWorkflow"] = isEntityInWorkflowInContext;   
-                    }
-                    break;
+                // case "IsEntityInWorkflowInContext":
+                //     let isEntityInWorkflowInContext = keywordCountCommonMethod(br , 'IsEntityInWorkflowInContext' , 1);
+                //     if (isEntityInWorkflowInContext !== null) {
+                //         obj["IsEntityInWorkflow"] = isEntityInWorkflowInContext;   
+                //     }
+                //     break;
 
-                case "InvokeWorkflow":
-                    let invokeWorkflow = keywordCountCommonMethod(br , 'InvokeWorkflow' , 5);
-                    if (invokeWorkflow !== null) {
-                        obj["InvokeWorkflow"] = invokeWorkflow;   
-                    }
-                    break;
+                // case "InvokeWorkflow":
+                //     let invokeWorkflow = keywordCountCommonMethod(br , 'InvokeWorkflow' , 5);
+                //     if (invokeWorkflow !== null) {
+                //         obj["InvokeWorkflow"] = invokeWorkflow;   
+                //     }
+                //     break;
 
-                case "ResumeWorkflow":
-                    let resumeWorkflow = ResumeWorkflow(br);
-                    if (resumeWorkflow !== null) {
-                        obj["ResumeWorkflow"] = resumeWorkflow;   
-                    }
-                    break;
+                // case "ResumeWorkflow":
+                //     let resumeWorkflow = ResumeWorkflow(br);
+                //     if (resumeWorkflow !== null) {
+                //         obj["ResumeWorkflow"] = resumeWorkflow;   
+                //     }
+                //     break;
 
-                case "ResumeWorkflowInContext":
-                    let resumeWorkflowInContext = keywordCountCommonMethod(br , 'ResumeWorkflowInContext' , 5);
-                    if (resumeWorkflowInContext !== null) {
-                        obj["ResumeWorkflowInContext"] = resumeWorkflowInContext;   
-                    }
-                    break;
+                // case "ResumeWorkflowInContext":
+                //     let resumeWorkflowInContext = keywordCountCommonMethod(br , 'ResumeWorkflowInContext' , 5);
+                //     if (resumeWorkflowInContext !== null) {
+                //         obj["ResumeWorkflowInContext"] = resumeWorkflowInContext;   
+                //     }
+                //     break;
 
-                case "ResumeRelatedEntityWorkflow":
-                    let resumeRelatedEntityWorkflow = ResumeRelatedEntityWorkflow(br);
-                    if (resumeRelatedEntityWorkflow !== null) {
-                        obj["ResumeRelatedEntityWorkflow"] = resumeRelatedEntityWorkflow;   
-                    }
-                    break;
+                // case "ResumeRelatedEntityWorkflow":
+                //     let resumeRelatedEntityWorkflow = ResumeRelatedEntityWorkflow(br);
+                //     if (resumeRelatedEntityWorkflow !== null) {
+                //         obj["ResumeRelatedEntityWorkflow"] = resumeRelatedEntityWorkflow;   
+                //     }
+                //     break;
 
-                case "GetRelatedEntityIds":
-                    let getRelatedEntityIds = GetRelatedEntityIds(br);
-                    if (getRelatedEntityIds !== null) {
-                        obj["GetRelatedEntityIds"] = getRelatedEntityIds;   
-                    }
-                    break;
+                // case "GetRelatedEntityIds":
+                //     let getRelatedEntityIds = GetRelatedEntityIds(br);
+                //     if (getRelatedEntityIds !== null) {
+                //         obj["GetRelatedEntityIds"] = getRelatedEntityIds;   
+                //     }
+                //     break;
 
-                case "GetRelatedEntityIdsForContext":
-                    let getRelatedEntityIdsForContext = GetRelatedEntityIdsForContext(br);
-                    if (getRelatedEntityIdsForContext !== null) {
-                        obj["GetRelatedEntityIdsForContext"] = getRelatedEntityIdsForContext;   
-                    }
-                    break;
+                // case "GetRelatedEntityIdsForContext":
+                //     let getRelatedEntityIdsForContext = GetRelatedEntityIdsForContext(br);
+                //     if (getRelatedEntityIdsForContext !== null) {
+                //         obj["GetRelatedEntityIdsForContext"] = getRelatedEntityIdsForContext;   
+                //     }
+                //     break;
 
-                case "GetRestAPIResponse":
-                    let getRestAPIResponse = GetRestAPIResponse(br);
-                    if (getRestAPIResponse !== null) {
-                        obj["GetRestAPIResponse"] = getRestAPIResponse;   
-                    }
-                    break;
+                // case "GetRestAPIResponse":
+                //     let getRestAPIResponse = GetRestAPIResponse(br);
+                //     if (getRestAPIResponse !== null) {
+                //         obj["GetRestAPIResponse"] = getRestAPIResponse;   
+                //     }
+                //     break;
 
-                case "CreateEntity":
-                    let createEntity = CreateEntity(br);
-                    if (createEntity !== null) {
-                        obj["CreateEntity"] = createEntity;   
-                    }
-                    break;
+                // case "CreateEntity":
+                //     let createEntity = CreateEntity(br);
+                //     if (createEntity !== null) {
+                //         obj["CreateEntity"] = createEntity;   
+                //     }
+                //     break;
 
-                case "DeleteEntity":
-                    let deleteEntity = DeleteEntity(br);
-                    if (deleteEntity !== null) {
-                        obj["DeleteEntity"] = deleteEntity;   
-                    }
-                    break;
+                // case "DeleteEntity":
+                //     let deleteEntity = DeleteEntity(br);
+                //     if (deleteEntity !== null) {
+                //         obj["DeleteEntity"] = deleteEntity;   
+                //     }
+                //     break;
 
-                case "CreateSnapshot":
-                    let createSnapshot = CreateSnapshot(br);
-                    if (createSnapshot !== null) {
-                        obj["CreateSnapshot"] = createSnapshot;   
-                    }
-                    break;
+                // case "CreateSnapshot":
+                //     let createSnapshot = CreateSnapshot(br);
+                //     if (createSnapshot !== null) {
+                //         obj["CreateSnapshot"] = createSnapshot;   
+                //     }
+                //     break;
 
-                case "RestoreSnapshot":
-                    let restoreSnapshot = RestoreSnapshot(br);
-                    if (RestoreSnapshot !== null) {
-                        obj["IsEntityInWorkflow"] = restoreSnapshot;   
-                    }
-                    break;
+                // case "RestoreSnapshot":
+                //     let restoreSnapshot = RestoreSnapshot(br);
+                //     if (RestoreSnapshot !== null) {
+                //         obj["IsEntityInWorkflow"] = restoreSnapshot;   
+                //     }
+                //     break;
 
-                case "SetEntityAttributeValue":
-                    let setEntityAttributeValue = SetEntityAttributeValue(br);
-                    if (setEntityAttributeValue !== null) {
-                        obj["SetEntityAttributeValue"] = setEntityAttributeValue;   
-                    }
-                    break;
+                // case "SetEntityAttributeValue":
+                //     let setEntityAttributeValue = SetEntityAttributeValue(br);
+                //     if (setEntityAttributeValue !== null) {
+                //         obj["SetEntityAttributeValue"] = setEntityAttributeValue;   
+                //     }
+                //     break;
 
-                case "SetEntityAttributeValueForContext":
-                    let setEntityAttributeValueForContext = SetEntityAttributeValueForContext(br);
-                    if (setEntityAttributeValueForContext !== null) {
-                        obj["SetEntityAttributeValueForContext"] = setEntityAttributeValueForContext;   
-                    }
-                    break;
+                // case "SetEntityAttributeValueForContext":
+                //     let setEntityAttributeValueForContext = SetEntityAttributeValueForContext(br);
+                //     if (setEntityAttributeValueForContext !== null) {
+                //         obj["SetEntityAttributeValueForContext"] = setEntityAttributeValueForContext;   
+                //     }
+                //     break;
 
-                case "AddEntityNestedAttributeRow":
-                    let addEntityNestedAttributeRow = AddEntityNestedAttributeRow(br);
-                    if (addEntityNestedAttributeRow !== null) {
-                        obj["AddEntityNestedAttributeRow"] = addEntityNestedAttributeRow;   
-                    }
-                    break;
+                // case "AddEntityNestedAttributeRow":
+                //     let addEntityNestedAttributeRow = AddEntityNestedAttributeRow(br);
+                //     if (addEntityNestedAttributeRow !== null) {
+                //         obj["AddEntityNestedAttributeRow"] = addEntityNestedAttributeRow;   
+                //     }
+                //     break;
 
-                case "SetRelatedEntityAttributeValue":
-                    let setRelatedEntityAttributeValue = SetRelatedEntityAttributeValue(br);
-                    if (setRelatedEntityAttributeValue !== null) {
-                        obj["SetRelatedEntityAttributeValue"] = setRelatedEntityAttributeValue;   
-                    }
-                    break;
+                // case "SetRelatedEntityAttributeValue":
+                //     let setRelatedEntityAttributeValue = SetRelatedEntityAttributeValue(br);
+                //     if (setRelatedEntityAttributeValue !== null) {
+                //         obj["SetRelatedEntityAttributeValue"] = setRelatedEntityAttributeValue;   
+                //     }
+                //     break;
 
-                case "SetRelatedEntityAttributeValueForContext":
-                    let setRelatedEntityAttributeValueForContext = SetRelatedEntityAttributeValueForContext(br);
-                    if (setRelatedEntityAttributeValueForContext !== null) {
-                        obj["SetRelatedEntityAttributeValueForContext"] = setRelatedEntityAttributeValueForContext;   
-                    }
-                    break;
+                // case "SetRelatedEntityAttributeValueForContext":
+                //     let setRelatedEntityAttributeValueForContext = SetRelatedEntityAttributeValueForContext(br);
+                //     if (setRelatedEntityAttributeValueForContext !== null) {
+                //         obj["SetRelatedEntityAttributeValueForContext"] = setRelatedEntityAttributeValueForContext;   
+                //     }
+                //     break;
 
-                case "GetEntityBusinessConditionStatus":
-                    let getEntityBusinessConditionStatus = GetEntityBusinessConditionStatus(br);
-                    if (getEntityBusinessConditionStatus !== null) {
-                        obj["GetEntityBusinessConditionStatus"] = getEntityBusinessConditionStatus;   
-                    }
-                    break;
+                // case "GetEntityBusinessConditionStatus":
+                //     let getEntityBusinessConditionStatus = GetEntityBusinessConditionStatus(br);
+                //     if (getEntityBusinessConditionStatus !== null) {
+                //         obj["GetEntityBusinessConditionStatus"] = getEntityBusinessConditionStatus;   
+                //     }
+                //     break;
 
-                case "GetEntityAttributeValueById":
-                    let getEntityAttributeValueById = GetEntityAttributeValueById(br);
-                    if (getEntityAttributeValueById !== null) {
-                        obj["GetEntityAttributeValueById"] = getEntityAttributeValueById;   
-                    }
-                    break;
+                // case "GetEntityAttributeValueById":
+                //     let getEntityAttributeValueById = GetEntityAttributeValueById(br);
+                //     if (getEntityAttributeValueById !== null) {
+                //         obj["GetEntityAttributeValueById"] = getEntityAttributeValueById;   
+                //     }
+                //     break;
 
-                case "GetEntityAttributeValueByIdInContext":
-                    let getEntityAttributeValueByIdInContext = GetEntityAttributeValueByIdInContext(br);
-                    if (getEntityAttributeValueByIdInContext !== null) {
-                        obj["GetEntityAttributeValueByIdInContext"] = getEntityAttributeValueByIdInContext;   
-                    }
-                    break;
+                // case "GetEntityAttributeValueByIdInContext":
+                //     let getEntityAttributeValueByIdInContext = GetEntityAttributeValueByIdInContext(br);
+                //     if (getEntityAttributeValueByIdInContext !== null) {
+                //         obj["GetEntityAttributeValueByIdInContext"] = getEntityAttributeValueByIdInContext;   
+                //     }
+                //     break;
 
-                case "SendEntityForGraphProcessing":
-                    let sendEntityForGraphProcessing = SendEntityForGraphProcessing(br);
-                    if (sendEntityForGraphProcessing !== null) {
-                        obj["SendEntityForGraphProcessing"] = sendEntityForGraphProcessing;   
-                    }
-                    break;
+                // case "SendEntityForGraphProcessing":
+                //     let sendEntityForGraphProcessing = SendEntityForGraphProcessing(br);
+                //     if (sendEntityForGraphProcessing !== null) {
+                //         obj["SendEntityForGraphProcessing"] = sendEntityForGraphProcessing;   
+                //     }
+                //     break;
 
-                case "SendWhereUsedEntitiesForGraphProcessing":
-                    let sendWhereUsedEntitiesForGraphProcessing = SendWhereUsedEntitiesForGraphProcessing(br);
-                    if (sendWhereUsedEntitiesForGraphProcessing !== null) {
-                        obj["SendWhereUsedEntitiesForGraphProcessing"] = sendWhereUsedEntitiesForGraphProcessing;   
-                    }
-                    break;
+                // case "SendWhereUsedEntitiesForGraphProcessing":
+                //     let sendWhereUsedEntitiesForGraphProcessing = SendWhereUsedEntitiesForGraphProcessing(br);
+                //     if (sendWhereUsedEntitiesForGraphProcessing !== null) {
+                //         obj["SendWhereUsedEntitiesForGraphProcessing"] = sendWhereUsedEntitiesForGraphProcessing;   
+                //     }
+                //     break;
 
                 case "InitiateExport":
                     let initiateExport = InitiateExportCommonMethod(br);
@@ -357,7 +357,6 @@ const GetEntityIds = (br) => {
     // console.log(childKeywordChecker(br , 'ITERATE[' , 'GetEntityIds'))
 
     if ((childKeywordChecker(br , 'ITERATE[' , 'GetEntityIds' )) === true) {
-        console.log('test')
         warningArray.push('This keyword is used inside ITERATE please make sure iteration count is not more than 5')
     }
 
@@ -408,7 +407,6 @@ const childKeywordChecker = (br , parent , child) => {
     }
 }
 
-
 const matchString = (str) =>{
     let stack = [];
     let flag = true;
@@ -449,5 +447,5 @@ const counter = (keyword , br) => {
     return br.split(kew).length-1;
 }
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 7071;
 app.listen(port, () => console.log('Server Started'))
